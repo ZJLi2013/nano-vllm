ad9778f20b0a895b6140089c113849c13626c77d
zjli2013 <leezhengjiang@126.com>
2025-10-16 19:49:33 +0800

    code note 1

--- nanovllm/layers/sampler.py
+++ nanovllm/layers/sampler.py
@@ -9,7 +9,19 @@ class Sampler(nn.Module):
 
     @torch.compile
     def forward(self, logits: torch.Tensor, temperatures: torch.Tensor):
-        logits = logits.float().div_(temperatures.unsqueeze(dim=1))
-        probs = torch.softmax(logits, dim=-1)
-        sample_tokens = probs.div_(torch.empty_like(probs).exponential_(1).clamp_min_(1e-10)).argmax(dim=-1)
+        logits = logits.float().div_(temperatures.unsqueeze(dim=1))  # 温度缩放
+        probs = torch.softmax(logits, dim=-1)  # softmax 概率
+        sample_tokens = probs.div_(
+            torch.empty_like(probs).exponential_(1).clamp_min_(1e-10)
+        ).argmax(
+            dim=-1
+        )  # Gumbel-max 采样
         return sample_tokens
+
+
+"""
+Gumbel-max 采样:
+    1. 每个采样点，初始化 exp(1) 作为 noise
+    2. probs/noise ~= log(probs) - log(noise) 
+    3. 取 argmax 返回
+"""
